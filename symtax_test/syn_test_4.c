f23184117e5268d27128107b00203e2769cde1a4
Yeonnnny <kdoy030@gmail.com>
2024-12-17 15:49:25 +0900

    필요없는 폴더 삭제함

--- symtax_test/syn_test_4.c
+++
@@ -1,9 +0,0 @@
-// 배열과 포인터 테스트
-int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int *ptr = arr;
-    for (int i = 0; i < 5; i++) {
-        printf("Element %d: %d\n", i, *(ptr + i));
-    }
-    return 0;
-}
